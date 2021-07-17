64a47c300dc213c0e31cc0916d717a24951f78a1
swatidt1693gmai <swatidt1693gmai@ip-172-31-68-246.ec2.internal>
2021-07-17 17:25:28 +0000

    THIS IS MY FIRST PUSH

---
+++ HelloWorld/src/ExInh.java
@@ -0,0 +1,13 @@
+public class ExInh{
+    public static void main(String[] args) {
+        System.out.println("hi");
+        try {
+        int a=50/0;//--->exception obj
+        }
+        catch(Exception e) {
+            System.out.println(e);//exception obj
+        }
+        System.out.println("hello");
+        
+    }
+}
